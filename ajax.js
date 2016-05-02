3f0ee7120993f4ba10937e9024c18bf45e6613d5
Astwen <astrid.wennberg@hotmail.se>
2016-05-02 12:49:54 +0200

    Create ajax.js

---
+++ ajax.js
@@ -0,0 +1,22 @@
+//Javascript med AJAX
+//Använda AJAX för att hämta innehållet på en hemsida
+
+//Vad är AJAX?
+//AJAX är en teknik, en kombination av saker, för att hämta data från URLs
+
+//Påbörja en hämting
+var request = new XMLHttpRequest();
+request.onreadystatechange = function() {
+ 
+ console.log("nåt hände");
+ 
+ console.log("jajemensan");
+ 
+ if(request.readyState == 4 && request.status == 200) {
+ console.log("Svaret (response) " + request.responseText);
+ 
+ //Spara text i variabel
+ var data = request.responseText;
+ document.write(data);
+ 
+ }
