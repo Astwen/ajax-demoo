5284896a333f882cb8f85176acc9829e1743b191
Astwen <astrid.wennberg@hotmail.se>
2016-05-02 12:53:34 +0200

    Update ajax.js

--- ajax.js
+++ ajax.js
@@ -7,19 +7,20 @@
 //Påbörja en hämting
 var request = new XMLHttpRequest();
 request.onreadystatechange = function() {
- 
- console.log("nåt hände");
- 
- console.log("jajemensan");
- 
- if(request.readyState == 4 && request.status == 200) {
- console.log("Svaret (response) " + request.responseText);
   
- //Spara text i variabel
- var data = request.responseText;
- document.write(data);
- 
- }
+  console.log("nåt hände");
+  
+  console.log("jajemensan");
+  
+  if(request.readyState == 4 && request.status == 200) {
+  console.log("Svaret (response) " + request.responseText);
+  
+  //Spara text i variabel
+  var data = request.responseText;
+  document.write(data);
+  
+  }
+  
 }
 
 request.open('GET', 'http://mardby.se/AJK15G/lorem_text.php');
